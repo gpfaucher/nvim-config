c956c8170cd1354053a3e07b2ceff930d327a78b
Gabriel Faucher <gabriel.faucher@axians.com>
2025-01-30 17:27:30 +0100

    feat: impl dynamic node_modules dir for angularls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
     { 'j-hui/fidget.nvim', opts = {} },
     {
       "folke/lazydev.nvim",
-      ft = "lua",   -- only load on lua files
+      ft = "lua", -- only load on lua files
       opts = {
         library = {
           -- See the configuration section for more details
@@ -47,18 +47,21 @@ return {
 
     -- These are just examples. Replace them with the language
     -- servers you have installed in your system
-    local cmd = { "./node_modules/.bin/ngserver", "--stdio", "--tsProbeLocations", "./node_modules",
-      "--ngProbeLocations", "./node_modules" }
+    local dynamic_node_module_dir = vim.fs.find(
+      { 'node_modules' },
+      { limit = math.huge, type = 'directory', stop = 'node_modules' }
+    )[1]
+
+    local cmd = { dynamic_node_module_dir .. "/.bin/ngserver", "--stdio", "--tsProbeLocations", "./node_modules",
+      "--ngProbeLocations", dynamic_node_module_dir }
 
     require 'lspconfig'.angularls.setup {
       cmd = cmd,
       on_new_config = function(new_config, new_root_dir)
         new_config.cmd = cmd
       end,
-      on_attach = on_attach,
     }
     lspconfig.lua_ls.setup({})
     lspconfig.ts_ls.setup {}
-    --require('lspconfig').ocamllsp.setup({})
   end
 }
