4e313419329d5470432248f63f0c3da143ed1ab8
Gabriel Faucher <gabriel.faucher@axians.com>
2025-02-02 00:03:32 +0100

    feat: added readme with todos

---
+++ README.md
@@ -0,0 +1,23 @@
+# nvim-config
+
+## TODO
+
+- [ ] Setup split/open keybinds for telescope
+- [ ] Setup markdown-oxide with Obsidian support
+- [ ] Setup bootstrap.sh script for arch linux / wsl
+- [ ] Setup a separate bootstrap script for all lsp servers, formatters etc.
+- [ ] Setup a plugin like easymotion to easily use f, t etc jumps
+- [ ] Fix completion keybinds (tab is next, enter is completion)
+- [ ] Add the following lsp:
+    - [ ] markdown lsp (marksman)
+    - [ ] sqlls
+    - [ ] powershell_es
+    - [ ] bashls
+    - [ ] implement biome instead of prettier
+    - [ ] csharp_with a good formatter
+    - [ ] cssls, sassls
+    - [ ] dockerls / docker-compose
+    - [ ] emmet
+    - [ ] pyright with black
+    - [ ] tailwindcss
+    - [ ] typescript
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,6 +14,13 @@ return {
         { limit = math.huge, type = 'directory', stop = 'node_modules' }
       )[1] or ''
 
+      vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+        pattern = { '*.bicep' },
+        callback = function()
+          vim.opt.ft = 'bicep'
+        end
+      })
+
       local cmd = { dynamic_node_module_dir .. "/.bin/ngserver", "--stdio", "--tsProbeLocations", "./node_modules",
         "--ngProbeLocations", dynamic_node_module_dir }
 
@@ -24,11 +31,27 @@ return {
         end,
       }
 
+      local bicep_lsp_bin = "/usr/local/bin/bicep-langserver/Bicep.LangServer.dll"
+      require 'lspconfig'.bicep.setup {
+        cmd = { "dotnet", bicep_lsp_bin }
+      }
+
+
+      require('lspconfig').yamlls.setup {
+        settings = {
+          yaml = {
+            schemas = {
+              ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = "Pipelines/*.yml",
+            },
+          },
+        }
+      }
+
       lspconfig.lua_ls.setup({})
       lspconfig.ts_ls.setup {}
+      require 'lspconfig'.jsonls.setup {}
     end
   },
-
   {
     'nvimdev/lspsaga.nvim',
     after = 'nvim-lspconfig',
@@ -46,7 +69,11 @@ return {
         end,
       })
 
-      require('lspsaga').setup({})
+      require('lspsaga').setup({
+        symbol_in_winbar = {
+          enable = false
+        }
+      })
     end,
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }
   },
