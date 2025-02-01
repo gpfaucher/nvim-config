1d5e4ecb5c3e7a5e3fe1573400ccbbd21ec913bb
Gabriel Faucher <gabriel.faucher@axians.com>
2025-02-01 22:50:36 +0100

    feat: impl lspsaga for better code navigation

--- init.lua
+++ init.lua
@@ -1,9 +1,1 @@
 require('config.lazy')
-
-
-local test_dirs = vim.fs.find(
-	{ 'node_modules' },
-	{ limit = math.huge, type = 'directory', stop = 'node_modules' }
-)[1]
-
-print(vim.inspect(test_dirs))
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,67 +1,62 @@
 return {
-  'neovim/nvim-lspconfig',
-  cmd = 'LspInfo',
-  event = { 'BufReadPre', 'BufNewFile' },
-  dependencies = {
-    -- Useful status updates for LSP.
-    { 'j-hui/fidget.nvim', opts = {} },
-    {
-      "folke/lazydev.nvim",
-      ft = "lua", -- only load on lua files
-      opts = {
-        library = {
-          -- See the configuration section for more details
-          -- Load luvit types when the `vim.uv` word is found
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        },
+  {
+    'neovim/nvim-lspconfig',
+    cmd = 'LspInfo',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      -- Useful status updates for LSP.
+      { 'j-hui/fidget.nvim', opts = {} },
+    },
+    config = function()
+      local lspconfig = require('lspconfig')
+      local dynamic_node_module_dir = vim.fs.find(
+        { 'node_modules' },
+        { limit = math.huge, type = 'directory', stop = 'node_modules' }
+      )[1] or ''
+
+      local cmd = { dynamic_node_module_dir .. "/.bin/ngserver", "--stdio", "--tsProbeLocations", "./node_modules",
+        "--ngProbeLocations", dynamic_node_module_dir }
+
+      lspconfig.angularls.setup {
+        cmd = cmd,
+        on_new_config = function(new_config)
+          new_config.cmd = cmd
+        end,
+      }
+
+      lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup {}
+    end
+  },
+
+  {
+    'nvimdev/lspsaga.nvim',
+    after = 'nvim-lspconfig',
+    config = function()
+      vim.api.nvim_create_autocmd('LspAttach', {
+        desc = 'LSP actions',
+        callback = function(event)
+          local opts = { buffer = event.buf, noremap = true }
+          vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<cr>', opts)
+          vim.keymap.set('n', 'gd', '<cmd>Lspsaga goto_definition<cr>', opts)
+          vim.keymap.set('n', 'gi', '<cmd>Lspsaga finder<cr>', opts)
+          vim.keymap.set('n', 'go', '<cmd>Lspsaga goto_type_definition<cr>', opts)
+          vim.keymap.set('n', 'gr', '<cmd>Lspsaga rename<cr>', opts)
+          vim.keymap.set('n', 'ca', '<cmd>Lspsaga code_action<cr>', opts)
+        end,
+      })
+
+      require('lspsaga').setup({})
+    end,
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }
+  },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       },
     },
   },
-  init = function()
-    -- Reserve a space in the gutter
-    -- This will avoid an annoying layout shift in the screen
-    vim.opt.signcolumn = 'yes'
-  end,
-  config = function()
-    local lspconfig = require('lspconfig')
-
-    -- LspAttach is where you enable features that only work
-    -- if there is a language server active in the file
-    vim.api.nvim_create_autocmd('LspAttach', {
-      desc = 'LSP actions',
-      callback = function(event)
-        local opts = { buffer = event.buf }
-
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-      end,
-    })
-
-    -- These are just examples. Replace them with the language
-    -- servers you have installed in your system
-    local dynamic_node_module_dir = vim.fs.find(
-      { 'node_modules' },
-      { limit = math.huge, type = 'directory', stop = 'node_modules' }
-    )[1]
-
-    local cmd = { dynamic_node_module_dir .. "/.bin/ngserver", "--stdio", "--tsProbeLocations", "./node_modules",
-      "--ngProbeLocations", dynamic_node_module_dir }
-
-    require 'lspconfig'.angularls.setup {
-      cmd = cmd,
-      on_new_config = function(new_config, new_root_dir)
-        new_config.cmd = cmd
-      end,
-    }
-    lspconfig.lua_ls.setup({})
-    lspconfig.ts_ls.setup {}
-  end
 }
