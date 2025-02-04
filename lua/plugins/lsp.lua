d7775c0f6dfd1ef4e17fcf0f68d9edf98ddfcbb9
Gabriel Faucher <gabriel.faucher@axians.com>
2025-02-04 21:26:03 +0100

    feat: migrated to mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,52 +6,51 @@ return {
     dependencies = {
       -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
     },
     config = function()
+      require('mason').setup {}
       local lspconfig = require('lspconfig')
-      local dynamic_node_module_dir = vim.fs.find(
-        { 'node_modules' },
-        { limit = math.huge, type = 'directory', stop = 'node_modules' }
-      )[1] or ''
+      local mason_lspconfig = require('mason-lspconfig')
 
-      vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
-        pattern = { '*.bicep' },
-        callback = function()
-          vim.opt.ft = 'bicep'
-        end
+      mason_lspconfig.setup({
+        ensure_installed = { "lua_ls", "bicep", "csharp_ls", "ts_ls", "jsonls", "bashls", "marksman", "yamlls" }
       })
 
-      local cmd = { dynamic_node_module_dir .. "/.bin/ngserver", "--stdio", "--tsProbeLocations", "./node_modules",
-        "--ngProbeLocations", dynamic_node_module_dir }
-
-      lspconfig.angularls.setup {
-        cmd = cmd,
-        on_new_config = function(new_config)
-          new_config.cmd = cmd
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup {}
         end,
-      }
+        ["angularls"] = function()
+          local dynamic_node_module_dir = vim.fs.find(
+            { 'node_modules' },
+            { limit = math.huge, type = 'directory', stop = 'node_modules' }
+          )[1] or ''
 
-      local bicep_lsp_bin = "/usr/local/bin/bicep-langserver/Bicep.LangServer.dll"
-      lspconfig.bicep.setup {
-        cmd = { "dotnet", bicep_lsp_bin }
-      }
+          local cmd = { dynamic_node_module_dir .. "/.bin/ngserver", "--stdio", "--tsProbeLocations", "./node_modules",
+            "--ngProbeLocations", dynamic_node_module_dir }
 
-      require('lspconfig').yamlls.setup {
-        settings = {
-          yaml = {
-            schemas = {
-              ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = "Pipelines/*.yml",
-            },
-          },
-        }
-      }
+          lspconfig.angularls.setup {
+            cmd = cmd,
+            on_new_config = function(new_config)
+              new_config.cmd = cmd
+            end,
+          }
+        end,
+        ['bicep'] = function()
+          vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+            pattern = { '*.bicep' },
+            callback = function()
+              vim.opt.ft = 'bicep'
+            end
+          })
 
-      lspconfig.lua_ls.setup {}
-      lspconfig.ts_ls.setup {}
-      lspconfig.jsonls.setup {}
-      lspconfig.marksman.setup {}
-      lspconfig.bashls.setup {}
-      lspconfig.csharp_ls.setup {}
+          local bicep_lsp_bin =
+          'C:\\Users\\gabriel.faucher\\AppData\\Local\\nvim-data\\mason\\packages\\bicep-lsp\\extension\\bicepLanguageServer\\Bicep.LangServer.dll'
+          lspconfig.bicep.setup { cmd = { 'dotnet', bicep_lsp_bin } }
+        end,
+      })
     end
   },
   {
@@ -78,6 +77,13 @@ return {
       })
     end,
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }
+  },
+  {
+    "TheLeoP/powershell.nvim",
+    ---@type powershell.user_config
+    opts = {
+      bundle_path = vim.fn.stdpath "data" .. "/mason/packages/powershell-editor-services",
+    }
   },
   {
     "folke/lazydev.nvim",
